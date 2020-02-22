66bc1a0e5174f6211ad1959222610e31c17763e4
unknown <liutauras.gaidamavicius@gmail.com>
2020-02-23 00:24:18 +0200

    Added rotate toward movement for enemies

---
+++ MiamiSentinel/Assets/Scripts/Enemy/EnemyAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    private IMovementInput input;
+
+    void Awake()
+    {
+        input = GetComponent<IMovementInput>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(input.Horizontal, 0.0f, input.Vertical);
+        transform.LookAt(transform.position + moveDirection, Vector3.up);
+    }
+}
